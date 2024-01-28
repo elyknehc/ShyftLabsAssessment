abe6dc99875f41d34a61989f447e865a8107335b
Kyle Chen <kchen727@uwo.ca>
2024-01-28 11:25:34 -0500

    update main page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,8 +5,11 @@ export default function Home() {
 	return (
 		<div>
 			<Navbar />
-			<main className="flex min-h-screen flex-col items-center justify-between p-24">
-				<div>Welcome!</div>
+			<main className="flex flex-col items-center justify-between p-24">
+				<div>Welcome to the Student Result Management System!</div>
+				<div>
+					Click on any of the links on the navigation bar to get started.
+				</div>
 			</main>
 		</div>
 	);
